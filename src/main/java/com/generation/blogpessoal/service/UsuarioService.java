bc4cf57b984e02063a61d521ff94b609513f2499
Iago Zandone <iagozandone@gmail.com>
2025-06-23 11:52:57 -0300

    Atualiza lógica do UsuarioService e configurações do profile de produção

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -44,6 +44,7 @@ public class UsuarioService {
 		}
 
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
+		usuario.setId(null);
 
 		return Optional.ofNullable(usuarioRepository.save(usuario));
 	}
